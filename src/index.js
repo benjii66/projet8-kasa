df278f38c87b00ef59b1b6dcad6b6b7559c5da50
B.N.J <benjamin51.simon@gmail.com>
2023-11-16 21:16:09 +0100

    update the node

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import "./styles/main.scss";
 
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
