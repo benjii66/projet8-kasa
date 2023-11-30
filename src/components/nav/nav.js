6202dcb50bbb2da1ad9bb2c14b39f10763aed8f8
B.N.J <benjamin51.simon@gmail.com>
2023-11-30 13:17:36 +0100

    Fix small things
    still have the refresh delayed... :tired_face:

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Nav() {
-	return (
-
-		//return the navigation content, with 2 links to the homepage and about page
-		<nav className="nav-header">
-			<Link to="/" aria-label="Home Page"className="nav-header_link-home">
-				Accueil
-			</Link>
-			<Link to="/about" aria-label="About Page"className="nav-header_link-about">
-				A Propos
-			</Link>
-		</nav>
-	);
+  return (
+    <nav className="nav-header">
+      <NavLink to="/" aria-label="Home Page" className="nav-header_link-home" activeClassName="active">
+        Accueil
+      </NavLink>
+      <NavLink to="/about" aria-label="About Page" className="nav-header_link-about" activeClassName="active">
+        A Propos
+      </NavLink>
+    </nav>
+  );
 }
--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ErrorPage from "../../components/errorPage/errorPage";
 
 export default function Error() {
+  //redirection to the error page
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectTimer = setTimeout(() => {
+      navigate("/error"); 
+    }, 10); 
+
+    return () => clearTimeout(redirectTimer);
+  }, [navigate]);
 	return (
 		<>
 		{/* will display an Error Page when it's called */}
